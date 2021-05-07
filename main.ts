99d2f8952cb7a84275f4f1e57d15a39cfb203075
Roger O <43441351+RogerOM@users.noreply.github.com>
2021-05-07 18:15:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+let punts = 0
 basic.forever(function () {
-	
+    if (punts > 5) {
+        basic.showIcon(IconNames.Heart)
+    } else if (punts > 2) {
+        basic.showIcon(IconNames.Ghost)
+    } else {
+    	
+    }
 })
